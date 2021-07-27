d1f4645d17930de7fae361de55f6f09829b4c481
nizzebro <nizze@inbox.ru>
2021-07-27 03:05:21 +0300

    ru-wiki dump parsed succesfully

--- WiktCSV/processor_ru.cpp
+++ WiktCSV/processor_ru.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "processor_ru.h"
 #include <cctype>
 using namespace wiktcsv;
@@ -6,9 +6,6 @@ using namespace char_parsers;
 
 #include <iostream>
 
-enum {
-    PAGES, HEADERS, LINES, REM
-};
 
 bool ProcessorRu::process(const char* dir, const char* fileName) noexcept
 {
@@ -19,37 +16,50 @@ bool ProcessorRu::process(const char* dir, const char* fileName) noexcept
     s += ('\0');
     if (!XmlParser::openFile(s.data())) return false;
     s.erase(pos);
-    if(!XmlParser::FileWriter::openFiles(std::move(s), {"pages.csv","headers.csv","lines.csv",
-        "rem.xml"})) return false;
+    if(!XmlParser::FileWriter::openFiles(std::move(s), fileNames)) return false;
 
-    std::string title, id; // page title, id
-    std::string    rem;    // accumulator for pages of special namespaces (templates, categories etc)
+
+	std::string title, idPage; // page title, id
+	std::string rem;    // accumulator for special pages
+	std::size_t idWord = 0;
+
+	std::cout << "Processed pages/bytes:";
+	std::cout << '\n';
 
     while(next()) // for each item
-    {
-        auto name = getName();                 
-        if(isElement() && name == "page")      // page start
+    {                
+        if(isElement("page"))      // <page> start
         {
-            rem = text;                // assign the first tag to non-vocab pages accumulator
+            rem = text;                // accumulate anything until <ns> says which namespace this is
             auto i = getLevel();         
             while(next(i))                  // for each item in this page 
             {  
-                rem.append(text);           // append each item until <ns> says which namespace this is
+                rem.append(text);  
                 if(isText())            
                 {
-                    name = getName();
+                    auto name = getName();
                     if(name == "title") title = text;
                     else if (name == "ns") 
                     {
-                        if(text != "0") 
+                        if(text != "0")     // special page (template, category, etc); append it to to rem.xml
                         {
-                            while(next(i)) rem.append(text);
-                            write(rem, REM);
+							ofstreams[REM] << rem; // flush accum...
+                            while(next(i))  // ... and the rest;
+                            {
+								ofstreams[REM] << text;
+								if (isElementEnd()) ofstreams[REM] << '\n';
+                            }
                             break;
                         }
                     }
-                    else if (name == "id") id = name;
-                    else if (name == "text") processText(title, id);
+                    else if (name == "id") idPage = text;
+                    else if (name == "redirect") break;     // skip any redirecting stub pages
+					else if (name == "text")
+					{
+						processText(idWord++, idPage, title); /// text always goes last; we have all vars collected
+						std::cout << "\r                       ";
+						std::cout << '\r' << idWord << '\t' << (getFilePos());
+					}
                 }
             }
         }
@@ -60,12 +70,149 @@ bool ProcessorRu::process(const char* dir, const char* fileName) noexcept
     return true;
 }
 
-void ProcessorRu::processText(std::string_view title, std::string_view id) noexcept
-{
-    charser it(text);
-    while (it.seek("\n= {{"), true)
-    {
-        int i = 0;
 
+
+
+
+
+void ProcessorRu::processText(std::size_t idWord, std::string_view idPage, std::string_view title) noexcept
+{
+	charser it(text); // block iterator
+	charser line;	// line it gets each time
+
+    bool ru = false;					// flag: ru-block				
+	std::string_view wordHeader = {};   // curr homograph; empty if there is no level 2
+	int subHeaderType = 0;				// sub-header id; values from file enum
+	int braceCount = 0;					// trace "{{...}}"
+
+    while(auto c = it.getspan(line, {'\r','\n' }, true))
+    { 
+		if(c == '\r') it.skip_if('\n'); // set up the pointer for the next line, in advance 
+
+		// parse the line; check for headers first
+
+		if (line.startsWith("= ")) // level 1 header; language
+		{
+			if (ru) break; // a ru-section just ended; exit loop to write the word if needed; 
+			ru = (line == ("= {{-ru-}} ="));
+			continue;
+		}
+
+		if (!ru) continue; // skip all non-ru lines - until a next level 1 header, if any
+
+		// ru section
+
+		if (!line) continue; // skip empty ones
+
+
+		if (line.startsWith("==")) // some header
+		{
+			if (braceCount) // unclosed multiline template link - should not happen, but...
+			{
+				ofstreams[subHeaderType] << '\n';
+				braceCount = 0;
+			}
+
+			subHeaderType = 0;
+
+			if (line.startsWith("== ")) // level 2 header: homograph title
+			{
+				line.skip(3);
+				line.getspan(line, " ==", false);
+				if (!wordHeader.empty()) // flash previous
+				{
+					ofstreams[WORDS] << idWord << '\t' << idPage << '\t' << title << '\t' << wordHeader << '\n';
+				}
+				
+				wordHeader = line;
+				continue;
+			}
+
+			line.seek(' ', true); 
+
+			if (line.startsWith(u8"Морф"))
+			{
+				subHeaderType = SYNTAX;
+				continue;
+			}
+			if (line.startsWith(u8"Этим"))
+			{
+				subHeaderType = ETIMOLOGY;
+				continue;
+			}
+			if (line.startsWith(u8"Фраз"))
+			{
+				subHeaderType = PHRASES;
+			    continue;
+			}
+			if (line.startsWith(u8"Посл"))
+			{
+				subHeaderType = SAYINGS;
+				continue;
+			}
+			if (line.startsWith(u8"Знач"))
+			{
+				subHeaderType = MEANINGS;
+				continue;
+			}	
+			continue;
+		}
+
+		if (line.startsWith(u8"&lt;!-- Служ"))
+		{
+			if (braceCount) // unclosed multiline template link - should not happen, but...
+			{
+				ofstreams[subHeaderType] << '\n';
+				braceCount = 0;
+			}
+			subHeaderType = CATEGORY;
+			continue;
+		}
+
+		// text line
+
+		if(!subHeaderType) continue;
+
+		if (!braceCount)
+		{
+			if (line == "* " || line == "# ")  continue;
+
+			if (subHeaderType == CATEGORY && !line.startsWith(u8"{{Категория")) continue;
+			if (subHeaderType == CATEGORY && line == u8"{{Категория|язык = ru|||}}") continue;
+
+		} 
+
+
+		auto n = 0; // local brace count
+
+		charser tmp(line);
+
+		while (auto c = tmp.getc()) // count braces and replace tabs
+		{
+			if(c == '{') ++n;
+			else if (c == '}') --n;
+			else if (c == '\t')
+			{
+				char* p = (char*)tmp.get() - 1;
+				*p = ' ';
+			}
+		}
+
+		
+		if (!braceCount) ofstreams[subHeaderType] << idWord << '\t';
+		ofstreams[subHeaderType] << line;
+		braceCount += n;
+		if (!braceCount) ofstreams[subHeaderType] << '\n';
     }
+
+	if (!ru) return; // no ru-section in this page
+	// write the last homograph (or single one if no level 2 headers, in this case wordHeader is just empty)
+	ofstreams[WORDS] << idWord << '\t' << idPage << '\t' << title << '\t' << wordHeader << '\n';
+
+	if (braceCount) // unclosed multiline template link - should not happen, but...
+	{
+		ofstreams[subHeaderType] << '\n';
+		braceCount = 0;
+	}
+
 }
--- WiktCSV/processor_ru.h
+++ WiktCSV/processor_ru.h
@@ -1,18 +1,57 @@
-#pragma once
+﻿#pragma once
 
 
 #include "processor.h"
 
-
 namespace wiktcsv {
+
+
+// Output data format.
+// The main goal is to get morphology and syntax.
+// Additionally, to extract etimology (except PIE bullshit), phrases and sayings -
+// even though ther is a lot of garbage there, word meaning definitions and maybe
+// english translations.
+// Template links are to be written as is and processed further in table fields.
+
+// Every other lang than "{{-ru-}}" will be thrown away.
+
+
+// Field delimiters - \t; Record delimiters = \n; in text, these should be replaced
+// by spaces.
+
+// Tables:
+
+// WORDS: homographs
+// IdWord(counter), IdPage(from xml), Title(from xml), Header; 
+// where Header is level 1 header string, e.g. "{{заголовок|I}}" -
+// or an empty field if there are no variations;
+
+// SYNTAX, ETIMOLOGY, PHRASES, SAYINGS, MEANINGS, CATEGORY: 
+// IdWord, Text
+// lines are lines only if there are formatting or entity that starts the line.
+
+// Special pages from namespaces other than 0 (templates, categories etc) will be
+// written as is to rem.xml.
+
 
 
 class ProcessorRu: XmlParser, XmlParser::FileWriter
 {
+	enum {
+		WORDS, SYNTAX, ETIMOLOGY, PHRASES, SAYINGS, MEANINGS, CATEGORY, REM
+	};
+
+	static constexpr std::initializer_list<const char*> fileNames =
+	{ "words.csv","syntax.csv","etimology.csv","phrases.csv","sayings.csv", "meanings.csv",
+		"category.csv","rem.xml" };
+
+	void processText(std::size_t idWord, std::string_view idPage, std::string_view title) noexcept;
+
+	XmlParser::FileWriter& ofstreams = static_cast<XmlParser::FileWriter&>(*this);
+
     public:
+
     bool process(const char* dir, const char* fileName) noexcept;
-    void processText(std::string_view title, std::string_view id) noexcept;
-    protected:
 
 };
 
